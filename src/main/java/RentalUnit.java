7f9e3eeebce4a272c888bce294b15357da065a5c
Linda Seiter <linseiter@gmail.com>
2023-02-10 18:39:56 -0500

    new lab constructor methods

---
+++ src/main/java/RentalUnit.java
@@ -0,0 +1,25 @@
+public class RentalUnit {
+    private String address;
+    private int monthlyRate;
+    private boolean petAllowed;
+    private int minimumMonths;
+
+    // Add constructor methods here
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getMonthlyRate() {
+        return monthlyRate;
+    }
+
+    public boolean isPetAllowed() {
+        return petAllowed;
+    }
+
+    public int getMinimumMonths() {
+        return minimumMonths;
+    }
+}
